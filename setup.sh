a9da7fa37c926a8aac9e3e39eda040794220ed22
Preston Evans <preston.b.evans@gmail.com>
2025-10-24 16:32:35 -0500

    Fix celestia script invocation

--- setup.sh
+++ setup.sh
@@ -262,17 +262,15 @@ echo "Journald configured."
 if [ "$SETUP_CELESTIA" = false ]; then
 	echo "Celestia parameters not provided, skipping Celestia setup"
 else
-    echo "Setting up Celestia light node with Quicknode as $TARGET_USER"
-	# TODO: determine genesis and config file paths
-  #	ROLLUP_GENESIS_FILE="/home/$TARGET_USER/rollup-starter/genesis/genesis.json"
-  #	ROLLUP_CONFIG_FILE="/home/$TARGET_USER/rollup-starter/rollup_config.toml"
+  echo "Setting up celestia"
+	# TODO: determine genesis and config file paths.
+	# This probably work, but needs to be double checked
+    ROLLUP_GENESIS_FILE="/home/$TARGET_USER/rollup-starter/configs/celestia/genesis.json"
+    ROLLUP_CONFIG_FILE="/home/$TARGET_USER/rollup-starter/configs/celestia/rollup_config.toml"
 
-	# Run the Celestia setup script
-	sg docker -c "bash \"$(dirname \"$0\")/setup_celestia_quicknode.sh\" \
-		\"$TARGET_USER\" \
-		\"$QUICKNODE_API_TOKEN\" \
-		\"$QUICKNODE_HOST\" \
-		\"$CELESTIA_KEY_SEED\""
+	# Run the Celestia setup script (use absolute path)
+	CELESTIA_SCRIPT="$(cd "$(dirname "$0")" && pwd)/setup_celestia_quicknode.sh"
+	sg docker -c "bash \"$CELESTIA_SCRIPT\" \"$TARGET_USER\" \"$QUICKNODE_API_TOKEN\" \"$QUICKNODE_HOST\" \"$CELESTIA_KEY_SEED\" \"$ROLLUP_GENESIS_FILE\" \"$ROLLUP_CONFIG_FILE\""
 fi
 
 
