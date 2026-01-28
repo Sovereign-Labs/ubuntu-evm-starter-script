8ac0e6f2cd7fbbb49c29d3670d9768141aa1ea80
Theodore Bugnet <theodore@sovlabs.io>
2026-01-28 17:02:20 +0000

    Make geoip blocking optional

--- setup_proxy_monitoring.sh
+++ setup_proxy_monitoring.sh
@@ -12,10 +12,17 @@ DEPLOYMENT_NAME="$6"
 GEOIP_ACCOUNT="$7"
 GEOIP_LICENSE="$8"
 
+GEOIP_ENABLED=0
+if [ -n "$GEOIP_ACCOUNT" ] && [ -n "$GEOIP_LICENSE" ]; then
+  GEOIP_ENABLED=1
+elif [ -n "$GEOIP_ACCOUNT" ] || [ -n "$GEOIP_LICENSE" ]; then
+  echo "WARNING: GeoIP disabled because both GEOIP_ACCOUNT and GEOIP_LICENSE are required."
+fi
+
 # Validate required parameters
-if [ -z "$MONITORING_URL" ] || [ -z "$INFLUX_TOKEN" ] || [ -z "$INFLUX_ORG" ] || [ -z "$INFLUX_BUCKET" ] || [ -z "$INSTANCE_ID" ] || [ -z "$DEPLOYMENT_NAME" ] || [ -z "$GEOIP_ACCOUNT" ] || [ -z "$GEOIP_LICENSE" ]; then
-  echo "Usage: $0 <MONITORING_URL> <INFLUX_TOKEN> <INFLUX_ORG> <INFLUX_BUCKET> <INSTANCE_ID> <DEPLOYMENT_NAME> <GEOIP_ACCOUNT> <GEOIP_LICENSE>"
-  echo "ERROR: Missing required monitoring or proxy parameters"
+if [ -z "$MONITORING_URL" ] || [ -z "$INFLUX_TOKEN" ] || [ -z "$INFLUX_ORG" ] || [ -z "$INFLUX_BUCKET" ] || [ -z "$INSTANCE_ID" ] || [ -z "$DEPLOYMENT_NAME" ]; then
+  echo "Usage: $0 <MONITORING_URL> <INFLUX_TOKEN> <INFLUX_ORG> <INFLUX_BUCKET> <INSTANCE_ID> <DEPLOYMENT_NAME> [GEOIP_ACCOUNT] [GEOIP_LICENSE]"
+  echo "ERROR: Missing required monitoring parameters"
   exit 1
 fi
 
@@ -24,28 +31,34 @@ yum groupinstall -y "Development Tools"
 # Use openssl11-devel for OpenSSL 1.1 (required by OpenResty 1.27+)
 yum install -y pcre-devel openssl11-devel zlib-devel perl perl-Data-Dumper git wget
 
-# Install libmaxminddb for the geoip2 module
-echo "Building libmaxminddb for the geoip module..."
-cd /tmp
-LIBMAXMINDDB_VERSION="1.12.2"
-wget "https://github.com/maxmind/libmaxminddb/releases/download/${LIBMAXMINDDB_VERSION}/libmaxminddb-${LIBMAXMINDDB_VERSION}.tar.gz"
-tar -xzf libmaxminddb-${LIBMAXMINDDB_VERSION}.tar.gz
-cd libmaxminddb-${LIBMAXMINDDB_VERSION}
-./configure
-make
-make install
-echo "/usr/local/lib"  >> /etc/ld.so.conf.d/local.conf
-ldconfig
+if [ "$GEOIP_ENABLED" -eq 1 ]; then
+  # Install libmaxminddb for the geoip2 module
+  echo "Building libmaxminddb for the geoip module..."
+  cd /tmp
+  LIBMAXMINDDB_VERSION="1.12.2"
+  wget "https://github.com/maxmind/libmaxminddb/releases/download/${LIBMAXMINDDB_VERSION}/libmaxminddb-${LIBMAXMINDDB_VERSION}.tar.gz"
+  tar -xzf libmaxminddb-${LIBMAXMINDDB_VERSION}.tar.gz
+  cd libmaxminddb-${LIBMAXMINDDB_VERSION}
+  ./configure
+  make
+  make install
+  echo "/usr/local/lib"  >> /etc/ld.so.conf.d/local.conf
+  ldconfig
 
-# Download GeoIP DBs using the helper script expected in /opt
-/opt/update-geoip-dbs.sh "$GEOIP_ACCOUNT" "$GEOIP_LICENSE"
-# And set up a cron job to refresh GeoIP DBs twice a week (Mon/Thu at 03:15)
-cat > /etc/cron.d/update-geoip-dbs <<EOF
+  # Download GeoIP DBs using the helper script expected in /opt
+  echo "Downloading MaxMind GeoIP2 database..."
+  /opt/update-geoip-dbs.sh "$GEOIP_ACCOUNT" "$GEOIP_LICENSE"
+  # And set up a cron job to refresh GeoIP DBs twice a week (Mon/Thu at 03:15)
+  cat > /etc/cron.d/update-geoip-dbs <<EOF
 SHELL=/bin/bash
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 15 3 * * 1,4 root /opt/update-geoip-dbs.sh "$GEOIP_ACCOUNT" "$GEOIP_LICENSE" >/var/log/geoip-db-update.log 2>&1
 EOF
-chmod 600 /etc/cron.d/update-geoip-dbs
+  chmod 600 /etc/cron.d/update-geoip-dbs
+  echo "Enabled bi-weekly database update cron."
+else
+  echo "Skipping GeoIP2 setup: GEOIP_ACCOUNT and GEOIP_LICENSE not both provided."
+fi
 
 echo "Building OpenResty with nginx-vts module..."
 
@@ -56,19 +69,25 @@ tar -xzf openresty-${OPENRESTY_VERSION}.tar.gz
 cd openresty-${OPENRESTY_VERSION}
 
 git clone --depth 1 https://github.com/vozlt/nginx-module-vts.git
-git clone --depth 1 https://github.com/leev/ngx_http_geoip2_module.git
+if [ "$GEOIP_ENABLED" -eq 1 ]; then
+  git clone --depth 1 https://github.com/leev/ngx_http_geoip2_module.git
+fi
 
 # Configure with OpenSSL 1.1 paths (Amazon Linux 2)
-./configure \
-  --prefix=/usr/local/openresty \
-  --with-http_v2_module \
-  --with-http_realip_module \
-  --with-http_gzip_static_module \
-  --with-http_stub_status_module \
-  --with-cc-opt="-I/usr/include/openssl11" \
-  --with-ld-opt="-L/usr/lib64/openssl11 -Wl,-rpath,/usr/lib64/openssl11" \
-  --add-module=./nginx-module-vts \
-  --add-module=./ngx_http_geoip2_module
+CONFIGURE_ARGS=(
+  --prefix=/usr/local/openresty
+  --with-http_v2_module
+  --with-http_realip_module
+  --with-http_gzip_static_module
+  --with-http_stub_status_module
+  --with-cc-opt="-I/usr/include/openssl11"
+  --with-ld-opt="-L/usr/lib64/openssl11 -Wl,-rpath,/usr/lib64/openssl11"
+  --add-module=./nginx-module-vts
+)
+if [ "$GEOIP_ENABLED" -eq 1 ]; then
+  CONFIGURE_ARGS+=(--add-module=./ngx_http_geoip2_module)
+fi
+./configure "${CONFIGURE_ARGS[@]}"
 
 make -j$(nproc)
 make install
